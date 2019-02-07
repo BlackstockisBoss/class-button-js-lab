02df15fb77e22c15745970a8b8edc1de18dce166
Jayden Blackstock <jayden.blackstock@sascholar.org>
2019-02-07 20:43:18 +0000

    I had given each div document.querySelectors I also used the event listener to add different ways the buttons will change the box and the text within the box

--- buttons.js
+++ buttons.js
@@ -1,5 +1,51 @@
 let popbutton = document.querySelector("#popbutton")
 
-popbutton.addEventListener("click", e => {
-    console.log("pop button clicked!")
+let target = document.querySelector("#target")
+
+popbutton.addEventListener('click' , e => {
+  if(target.classList.contains("pop")){
+    target.classList.remove("pop")
+  } else {
+    target.classList.add("pop")
+  }
+})
+
+let bigbutton = document.querySelector("#bigbutton")
+
+bigbutton.addEventListener("click", e => {
+  if(target.classList.contains("big")){
+    target.classList.remove("big")
+  } else {
+    target.classList.add("big")
+  }
+})
+
+let fancybutton = document.querySelector("#fancybutton")
+
+fancybutton.addEventListener("click", e => {
+  if(target.classList.contains("fancy")){
+    target.classList.remove("fancy")
+  } else {
+    target.classList.add("fancy")
+  }
+})
+
+let negativebutton = document.querySelector("#negativebutton")
+
+negativebutton.addEventListener("click", e => {
+  if(target.classList.contains("negative")){
+    target.classList.remove("negative")
+  } else {
+    target.classList.add("negative")
+  }
+})
+
+let custombutton = document.querySelector("#custombutton")
+
+custombutton.addEventListener("click", e => {
+  if(target.classList.contains("custom")){
+    target.classList.remove("custom")
+  } else {
+    target.classList.add("custom")
+  }
 })
